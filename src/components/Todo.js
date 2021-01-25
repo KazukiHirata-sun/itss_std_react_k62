9933d0fc6202deeb854fa5de21440bab72bc3a7f
EC2 Default User <ec2-user@ip-172-31-36-251.ap-southeast-1.compute.internal>
2021-01-25 04:46:54 +0000

    mod done state of ToDo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,13 +10,27 @@ function Todo() {
     { key: getKey(), text: 'Get some good sleep', done: false },
   ]);
   
+  const handleCheck = checked => {
+    const newItems = items.map(item => {
+      if (item.key === checked.key) {
+        item.done = !item.done;
+      }
+      return item;
+    });
+    setItems(newItems);
+  };
+  
   return (
     <div className="panel">
       <div className="panel-heading">
         ITSS1 ToDoアプリ
       </div>
        {items.map(item => (
-         <TodoItem key={item.key} item={item} />
+        <TodoItem
+          key={item.key}
+          item={item}
+          onCheck={handleCheck}
+        />
        ))}
       <div className="panel-block">
         {items.length} items
